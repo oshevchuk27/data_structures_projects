894fc0292e27c9259a715611e28dc01ebaecb1cd
Olga Shevchuk <Olga Shevchuk@165.82.201.239>
2020-01-24 12:30:23 -0500

    Class work

--- src/basic/Main.java
+++ src/basic/Main.java
@@ -3,6 +3,7 @@ package basic;
 public class Main {
 
 	public static void main(String[] args) {
+		power(3.5, 4);
 		/*
 		TODO: You should call the functions you create from this function in order
 		to test them. You can check that your Eclipse setup is correct by running
@@ -11,8 +12,25 @@ public class Main {
 		*/
 		System.out.println("Welcome to CS 106!  Enjoy Lab 0!");
 	}
+public static double power(double x, int exp) {
+	int number = 1;
+	for (int i = 0; i<exp; i++) {
+		number = number*x;  
+	}
+	return (number);
+}
+public static int GCD(int n, int m) {
+	int max = 0;
+	if ()
+}
+public static boolean isPrime (int value) {
+	for 
+}
+public static int round(double number1) {
+	
+}
 
-	// TODO: You should add the functions you create for the lab here and below.
+// TODO: You should add the functions you create for the lab here and below.
 
 
 }
